8ea7384108b9ae94bd1562cf73264ad4eb73321c
Vitaliia <vitaliia0907@gmail.com>
2025-10-06 17:58:35 +0200

    Modified styles for form

---
+++ src/js/2-form.js
@@ -0,0 +1,44 @@
+  let formData = {
+  email: "",
+  message: ""
+};
+
+
+const STORAGE_KEY = "feedback-form-state";
+
+
+const form = document.querySelector(".feedback-form");
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+  formData.email = parsedData.email || "";
+  formData.message = parsedData.message || "";
+  emailInput.value = formData.email;
+  messageInput.value = formData.message;
+}
+
+form.addEventListener("input", (event) => {
+  const { name, value } = event.target;
+  if (name === "email" || name === "message") {
+    formData[name] = value.trim(); // обрізаємо пробіли з країв
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: "", message: "" };
+  form.reset();
+});
